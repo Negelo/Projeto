846d8c3ff4dd7a90010123a2cc5c655e3807b218
angelo <angelo.alves.2025111@my.istec.pt>
2026-05-22 17:11:09 +0100

    Desenvolvimento das páginas principais

---
+++ projeto/main/urls.py
@@ -0,0 +1,8 @@
+﻿from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('publicacoes/', views.publicacoes, name='publicacoes'),
+    path('utilizadores/', views.utilizadores, name='utilizadores'),
+]
--- projeto/main/views.py
+++ projeto/main/views.py
@@ -1,2 +1,13 @@
 ﻿from django.shortcuts import render
 
+
+def home(request):
+    return render(request, 'main/home.html')
+
+
+def publicacoes(request):
+    return render(request, 'main/publicacoes.html')
+
+
+def utilizadores(request):
+    return render(request, 'main/utilizadores.html')
